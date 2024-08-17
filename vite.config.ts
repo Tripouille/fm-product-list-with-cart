94fedf516e5c79a0813e2e723908fafb8420e13e
jgambard <jmichel.gambard@gmail.com>
2024-08-17 14:44:19 +0200

    chore: remove vite-tsconfig-paths

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,13 @@
 import vue from "@vitejs/plugin-vue";
+import path from "node:path";
 import { defineConfig } from "vite";
-import tsconfigPaths from "vite-tsconfig-paths";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [tsconfigPaths(), vue()],
+  plugins: [vue()],
+  resolve: {
+    alias: {
+      "@": path.resolve("./src"),
+    },
+  },
 });
