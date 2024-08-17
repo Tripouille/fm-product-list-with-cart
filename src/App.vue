fc4210f905416cc8a96978a9c82ac94470434861
jgambard <jmichel.gambard@gmail.com>
2024-08-17 12:43:10 +0200

    feat: create product card

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,38 @@
 <script setup lang="ts">
+import ProductCard from "./components/ProductCard.vue";
+import { useProducts } from "./composables/useProducts";
+const { products } = useProducts();
 </script>
 
 <template>
   <main>
-    <h1>Hello</h1>
+    <h1>Desserts</h1>
+    <ul>
+      <li v-for="product in products">
+        <ProductCard v-bind="product" />
+      </li>
+    </ul>
   </main>
 </template>
 
-<style scoped></style>
+<style scoped>
+h1 {
+  margin-bottom: 1.5rem;
+}
+main {
+  max-width: 76rem;
+  margin: 0 auto;
+  padding: 5.5rem 7rem;
+}
+ul {
+  max-width: 50rem;
+  display: flex;
+  flex-direction: row;
+  flex-wrap: wrap;
+  gap: 1.5rem;
+
+  li {
+    list-style: none;
+  }
+}
+</style>
---
+++ src/components/ProductCard.vue
@@ -0,0 +1,50 @@
+<script setup lang="ts">
+import { Product } from "@/repositories/productRepository";
+
+const props = defineProps<Product>();
+</script>
+
+<template>
+  <figure>
+    <img
+      :src="props.image.desktop"
+      :alt="props.name"
+      height="262"
+      width="250"
+    />
+    <figcaption>
+      <p class="category text-preset-4">{{ props.category }}</p>
+      <h2 class="name text-preset-3">{{ props.name }}</h2>
+      <p class="price text-preset-3">{{ props.price }}</p>
+    </figcaption>
+  </figure>
+</template>
+
+<style lang="scss" scoped>
+figure {
+  width: 15.625rem;
+
+  img {
+    border-radius: 0.5rem;
+    margin-bottom: 1rem;
+  }
+
+  figcaption {
+    display: flex;
+    flex-direction: column;
+    gap: 0.25rem;
+
+    .category {
+      color: var(--clr-rose-500);
+    }
+
+    .name {
+      color: var(--clr-rose-900);
+    }
+
+    .price {
+      color: var(--clr-red);
+    }
+  }
+}
+</style>
