51025652de9392c9465e8ef8c00d797f9578da2b
jgambard <jmichel.gambard@gmail.com>
2024-08-21 18:29:00 +0200

    feat: display empty cart in shopping cart

--- src/components/ShoppingCart.vue
+++ src/components/ShoppingCart.vue
@@ -7,6 +7,13 @@ const { totalQuantity } = useShoppingCart();
 <template>
   <aside>
     <h2 class="text-preset-2">Your Cart ({{ totalQuantity }})</h2>
+    <section class="empty-car" v-if="!totalQuantity">
+      <img src="/assets/images/illustration-empty-cart.svg" alt="empty cart" />
+      <p class="empty-car__message text-preset-4-bold">
+        Your added items will appear here
+      </p>
+    </section>
+    <section v-else></section>
   </aside>
 </template>
 
@@ -16,6 +23,18 @@ aside {
 
   h2 {
     color: var(--clr-red);
+    margin-bottom: var(--spc-300);
+  }
+
+  .empty-car {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    padding: var(--spc-200) 0;
+    &__message {
+      color: var(--clr-rose-500);
+      text-align: center;
+    }
   }
 }
 </style>
