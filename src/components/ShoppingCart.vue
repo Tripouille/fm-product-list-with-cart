4009db3f3f96eaf98a1a95f8cba8f12ba71e2549
jgambard <jmichel.gambard@gmail.com>
2024-08-21 18:14:47 +0200

    feat: display total quantity in shopping cart

--- src/components/ShoppingCart.vue
+++ src/components/ShoppingCart.vue
@@ -1,9 +1,21 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { useShoppingCart } from "@/composables/useShoppingCart";
+
+const { totalQuantity } = useShoppingCart();
+</script>
 
 <template>
   <aside>
-    <h2>Your Cart (7)</h2>
+    <h2 class="text-preset-2">Your Cart ({{ totalQuantity }})</h2>
   </aside>
 </template>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+aside {
+  width: 100%;
+
+  h2 {
+    color: var(--clr-red);
+  }
+}
+</style>
